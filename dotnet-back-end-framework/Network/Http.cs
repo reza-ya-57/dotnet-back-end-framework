8996a5f186d41ac4e0b503bf675f9bb788dbffb2
reza-ya-57 <rezaayaari.57@gmail.com>
2025-02-15 20:24:00 +0330

    feat: receive my first message from socket successfully :)

---
+++ dotnet-back-end-framework/Network/Http.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet_back_end_framework.Network
+{
+    internal static class Http
+    {
+        public static void SendHttpRequest(Uri? uri = null, int port = 80)
+        {
+            uri ??= new Uri("http://example.com");
+
+            // Construct a minimalistic HTTP/1.1 request
+            byte[] requestBytes = Encoding.ASCII.GetBytes(@$"GET {uri.AbsoluteUri} HTTP/1.0
+                                                                Host: {uri.Host}
+                                                                Connection: Close
+
+                                                                ");
+
+            // Create and connect a dual-stack socket
+            using Socket socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(uri.Host, port);
+            
+
+            // Send the request.
+            // For the tiny amount of data in this example, the first call to Send() will likely deliver the buffer completely,
+            // however this is not guaranteed to happen for larger real-life buffers.
+            // The best practice is to iterate until all the data is sent.
+            int bytesSent = 0;
+            while (bytesSent < requestBytes.Length)
+            {
+                bytesSent += socket.Send(requestBytes, bytesSent, requestBytes.Length - bytesSent, SocketFlags.None);
+            }
+
+            // Do minimalistic buffering assuming ASCII response
+            byte[] responseBytes = new byte[256];
+            char[] responseChars = new char[256];
+
+            while (true)
+            {
+                int bytesReceived = socket.Receive(responseBytes);
+
+                // Receiving 0 bytes means EOF has been reached
+                if (bytesReceived == 0) break;
+
+                // Convert byteCount bytes to ASCII characters using the 'responseChars' buffer as destination
+                int charCount = Encoding.ASCII.GetChars(responseBytes, 0, bytesReceived, responseChars, 0);
+
+                // Print the contents of the 'responseChars' buffer to Console.Out
+                Console.Out.Write(responseChars, 0, charCount);
+            }
+        }
+
+    }
+
+}
--- dotnet-back-end-framework/Program.cs
+++ dotnet-back-end-framework/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace dotnet_back_end_framework
+﻿using dotnet_back_end_framework.Network;
+
+namespace dotnet_back_end_framework
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Http.SendHttpRequest(uri: new Uri("http://rezayari.ir") , 80);
+
+            var server = new Server();
+
+            server.Listen();
         }
     }
 }
