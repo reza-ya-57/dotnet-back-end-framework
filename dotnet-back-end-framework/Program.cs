df1313a16ddfe6891e6c14b61bf9ba619d038e42
reza-ya-57 <rezaayaari.57@gmail.com>
2025-02-15 18:31:12 +0330

    Add project files.

---
+++ dotnet-back-end-framework/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace dotnet_back_end_framework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
